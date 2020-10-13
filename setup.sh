8643a183264ce4b5ebbb9689268c66e1f1bc0035
Matthew Wolff <mm.wolff@chi-squared.org>
2020-10-13 02:21:20 -0500

    FIX: setup.sh wasn't universal
    Former-commit-id: 8a8b75b3f7179d573fc270552e38bf27460dc84a

--- setup.sh
+++ setup.sh
@@ -5,7 +5,11 @@ set -o xtrace
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
+# grab dependencies
+builtin cd "$DIR"
 git submodule update --init --recursive
+
+# build and run
 export GOPATH="$DIR"
 builtin cd "$DIR/src/app"
 go build
